92c40751a119b4b7f62a2e2554a008e4188d09fa
Anonxchange <104064161+Anonxchange@users.noreply.github.com>
2025-09-18 22:29:31 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import { CartProvider } from "@/contexts/CartContext";
 import Index from "./pages/Index";
 import MenuPage from "./pages/MenuPage";
 import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import GiftCardPage from "./pages/GiftCardPage";
+import ReservationPage from "./pages/ReservationPage";
 import Checkout from "./pages/Checkout";
 import NotFound from "./pages/NotFound";
 
@@ -23,6 +26,10 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/menu" element={<MenuPage />} />
             <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/gift-cards" element={<GiftCardPage />} />
+            <Route path="/info/gift-cards" element={<GiftCardPage />} />
+            <Route path="/reservations" element={<ReservationPage />} />
             <Route path="/checkout" element={<Checkout />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
